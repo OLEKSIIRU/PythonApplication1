2b3c8c5f1f4abea5951479c967d4ab1c174cc871
OLUKSIIRU <oleksiirudenkox@gmail.com>
2023-11-16 12:09:16 +0200

    1

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -7,3 +7,5 @@ print("Tere tulemast kooli "+kool.upper+"!\nOle hea"+str(kursus)+".kuursuse õpil
 print("Tere tulemst kooli", kool.lower"!\nOle hea",kursus,".kuursuse õpilaseks!")#Kool on kool
 
 print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!". format(kool,kursus))#kool on Kool
+print(type(kool))
+print(type(kursus))
