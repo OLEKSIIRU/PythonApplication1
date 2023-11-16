37b06e49ff9e639a69838e998c07e4568617edec
OLUKSIIRU <oleksiirudenkox@gmail.com>
2023-11-16 12:07:57 +0200

    1

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,9 @@
 print   ("Tere tulemast!".center(50))
-kool=input("\tMis koolis sa õpid?: ") #str kool
+kool=input("\tMis koolis sa õpid?: ").capitalize #str Kool
 kursus=int(input("\tMis kursusel?: ")) #int kursus
-print("Tere tulemast kooli "+kool+"!\nOle hea"+str(kursus)+".kuursuse õpilaseks!")
-print("Tere tulemst kooli", kool,"!\nOle hea",kursus,".kuursuse õpilaseks!")
-print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!". format(kool,kursus))
+
+print("Tere tulemast kooli "+kool.upper+"!\nOle hea"+str(kursus)+".kuursuse õpilaseks!") #kool on KOOl
+
+print("Tere tulemst kooli", kool.lower"!\nOle hea",kursus,".kuursuse õpilaseks!")#Kool on kool
+
+print("Tere tulemast kooli {0}!\nOle hea {1}.kuursuse õpetajaks!". format(kool,kursus))#kool on Kool
