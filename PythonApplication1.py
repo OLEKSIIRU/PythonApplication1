05000a6cf593ca82e08f9181f426d10a96b50564
OLUKSIIRU <oleksiirudenkox@gmail.com>
2023-11-16 15:13:19 +0200

    7

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,4 +1,44 @@
-﻿print   ("Tere tulemast!".center(50))
+﻿#1
+print("Tere mailm")
+nimi=input("Mis on sinu nimi on?")
+print("Tere mailm! Tervitan sind{0}!".format(nimi))
+vanus=int(input("Kui vana sa oled?"))
+print("Tere, maailm! Tervitan sind {0}! aastat vana.".format(nimi, vanus))
+
+#2
+
+vanu=18 #type(vanus) -> int
+eesnimi ="Jääk" #type(eesnimi) -> str
+pikkus=16.5 #type(pikkkus) -> float
+kas_käib_koolis= True #type(kas_käib_koolis) -> bool
+#3
+candy=int(input("Laual olevate kommide ar:"))
+print("Laua peal on{0}".format(candy))
+mitu=int(input("Mitu kommi sa soovid süüa:"))
+candy-=mitu
+print("Nüüd laua peal on {0}".format(candy))
+
+#4
+D=float(input("dlinna okruzhnosti "))
+print("diametr on {0}".fromat(D/3.14))
+
+#5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print   ("Tere tulemast!".center(50))
 kool=input("\tMis koolis sa õpid?: ").capitalize #str Kool
 kursus=int(input("\tMis kursusel?: ")) #int kursus
 
